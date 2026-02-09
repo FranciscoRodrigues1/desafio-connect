e66d0e23643f0aaa3fca049c15b8feb9f3ff6714
FranciscoRodrigues1 <franciscorodrigues.dev@gmail.com>
2026-02-09 00:18:34 -0300

    concluindo a pagina Ações da Connect

--- src/components/AcoesDaConnect/AcoesDaConnect.jsx
+++ src/components/AcoesDaConnect/AcoesDaConnect.jsx
@@ -1,10 +1,85 @@
-import React from "react";
+import ImgCards from "../../assets/mutirao.png";
+import ImgAulas from "../../assets/aulas.png";
+import ImgEsporteInclusao from "../../assets/esporte.png";
+import ImgGrandeFamilia from "../../assets/familia.png";
+import ImgFuturoNaEscola from "../../assets/futuro.png";
+import ImgConectJovem from "../../assets/conecta.png";
+
+import styles from "./AcoesDaConnect.module.scss";
 
 function AcoesDaConnect() {
   return (
-    <>
-      <h2>Eu sou a Ações da connect </h2>
-    </>
+    <section className={styles.secaoCards}>
+      <div>
+        <h1>Ações da Connect</h1>
+        <p>
+          Junte-se a quem acredita na mudança. Apoie, participe e transforme
+          vidas através de ações sociais que geram impacto real nas comunidades.
+        </p>
+      </div>
+
+      <section className={styles.cards}>
+        <article>
+          <img src={ImgCards} alt="" />
+            <h2>Mutirão de reciclagem</h2>
+            <p>
+              Coletar materiais recicláveis e orientar sobre descarte
+              consciente.
+            </p>
+            <button>Quero participar</button>
+        </article>
+
+        <article>
+          <img src={ImgAulas} alt="" />
+          <h2>Aulas de Tecnologia</h2>
+          <p>
+            Ensinar noções básicas de informática, internet segura e
+            programação. Preparar jovens para o mercado de trabalho digital.
+          </p>
+          <button>Quero participar</button>
+        </article>
+
+        <article>
+          <img src={ImgEsporteInclusao} alt="" />
+          <h2>Esporte e Inclusão</h2>
+          <p>
+            Organizar treinos e jogos que incentivem a participação de todos.
+            Promover disciplina, saúde e trabalho em equipe entre jovens.
+          </p>
+          <button>Quero participar</button>
+        </article>
+
+        <article>
+          <img src={ImgGrandeFamilia} alt="" />
+          <h2>Instituto grande familia</h2>
+          <p>
+            Contribua com alimentos não perecíveis e ajude famílias em situação
+            de vulnerabilidade.
+          </p>
+          <button>Quero participar</button>
+        </article>
+
+        <article>
+          <img src={ImgFuturoNaEscola} alt="" />
+          <h2>Projeto Futuro na Escola</h2>
+          <p>
+            Doe livros, cadernos, lápis, mochilas e canetas para ajudar jovens a
+            continuarem seus estudos com mais estrutura.
+          </p>
+          <button>Quero participar</button>
+        </article>
+
+        <article>
+          <img src={ImgConectJovem} alt="" />
+          <h2>Instituto Conecta Jovem</h2>
+          <p>
+            Doe computadores usados, tablets ou celulares em bom estado para
+            permitir que jovens tenham acesso ao mundo digital.
+          </p>
+          <button>Quero participar</button>
+        </article>
+      </section>
+    </section>
   );
 }
 
--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import styles from "./Perfil.module.scss";
+import ImgPerfil from "../../assets/perfil.png";
 
 function Perfil() {
   return (
-    <>
-      <h2>Perfil</h2>
-    </>
+    <section className={styles.containerTelaPerfil}>
+      <article>
+        <img src={ImgPerfil} alt="Foto de perfil" />
+        <div className={styles.cardInfo}>
+          <h2> Francisco Rodrigues </h2>
+          <h3> Voluntário Ativo</h3>
+          <p className={styles.paragrafoSobreMim}>
+            Apaixonado por fazer a diferença na comunidade. Acredito que
+            pequenas ações podem transformar vidas e estou sempre em busca de
+            novas oportunidades para ajudar.
+          </p>
+          <address>
+            <p> Camocim, CE</p>
+            <p>franciscorodrigues.dev@gmai.com</p>
+            <p> Membro desde outubro de 2025</p>
+          </address>
+          <ul className={styles.listaInteresses}>
+            <li>Educação</li>
+            <li>Meio Ambiente</li>
+            <li>Assistência Social</li>
+            <li>Design</li>
+          </ul>
+        </div>
+      </article>
+    </section>
   );
 }
-
 export default Perfil;
