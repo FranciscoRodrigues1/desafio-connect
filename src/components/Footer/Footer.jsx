95817adb71053f789e1fcc633c7887fd9c5eff9a
FranciscoRodrigues1 <franciscorodrigues.dev@gmail.com>
2026-02-09 01:21:33 -0300

    crie o Footer e integrei a landpage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,30 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ImgFooter from "../../assets/logo.png";
+import ImgLinkedin from "../../assets/linkedin.png";
+import ImgInstagran from "../../assets/instagram.png";
+import ImgWhatsapp from "../../assets/whatsapp.png";
+import Styles from "./footer.module.scss";
 
 function Footer() {
-    return (
-        <>
-        
-        <h2>Sou o footer</h2>
-        </>
-    )
+  return (
+    <section className={Styles.sectioFooter}>
+      <div className={Styles.divFooter}>
+        <img src={ImgFooter} alt="Imagem da logo da página" />
+        <h2>Connect</h2>
+      </div>
+
+      <p>
+        © 2026 Vai na Web & Empower. Todos os direitos reservados. Projeto
+        destinado exclusivamente a fins educativos.
+      </p>
+
+      <section className={Styles.redesSociais}>
+        <img src={ImgLinkedin} alt="imagem da rede social Linkedin" />
+        <img src={ImgInstagran} alt="imagem da rede social instagram" />
+        <img src={ImgWhatsapp} alt="imagem da rede social whatsapp" />
+      </section>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
