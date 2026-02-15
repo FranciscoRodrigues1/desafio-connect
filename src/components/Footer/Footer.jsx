27ac1945829f5ddb680135db5f4f1333d2a9d6b5
FranciscoRodrigues1 <franciscorodrigues.dev@gmail.com>
2026-02-15 02:01:37 -0300

    inserindo elementos ao footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import ImgFooter from "../../assets/logo.png";
 import ImgLinkedin from "../../assets/linkedin.png";
 import ImgInstagran from "../../assets/instagram.png";
 import ImgWhatsapp from "../../assets/whatsapp.png";
+import Github from "../../assets/Github.png"
 import Styles from "./Footer.module.scss";
 
 function Footer() {
@@ -18,9 +19,10 @@ function Footer() {
       </p>
 
       <section className={Styles.redesSociais}>
-        <img src={ImgLinkedin} alt="imagem da rede social Linkedin" />
-        <img src={ImgInstagran} alt="imagem da rede social instagram" />
-        <img src={ImgWhatsapp} alt="imagem da rede social whatsapp" />
+        <a href="https://www.linkedin.com/in/franciscorodriguesdev/"  target="_blank"><img src={ImgLinkedin} alt="imagem da rede social Linkedin" /></a>
+        <a href="https://github.com/FranciscoRodrigues1" target="_blank"><img src={Github} alt="Imagem da rede social Github" /></a>
+        <a href="https://www.instagram.com/keltonrodrigues_/" target="_blank"><img src={ImgInstagran} alt="imagem da rede social instagram" /></a>
+        <a href="https://wa.me/+5588992765611 " target="_blank"><img src={ImgWhatsapp} alt="imagem da rede social whatsapp" /></a>
       </section>
     </footer>
   );
